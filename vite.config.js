5be3cf3aabc9b25f9483db26352420fa64ea2c0f
Arti <artimarkali37@gmail.com>
2026-04-09 10:33:47 +0530

    vite config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,14 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ["**/*.mp4"],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  server: {
+    cors: true,
+  },
 });
